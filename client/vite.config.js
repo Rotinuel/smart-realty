2092cb952219c2809c0ee742868bc482a2786748
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-10-18 12:47:50 +0100

    4

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import lineClamp from '@tailwindcss/line-clamp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +12,5 @@ export default defineConfig({
     },
   },
   plugins: [
-    lineClamp],
+    react()],
 })
